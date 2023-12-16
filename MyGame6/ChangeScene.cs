43293b744599d3f08366569226c7fd41834f626e
itn3000 <itn3000@gmail.com>
2023-12-17 03:34:39 +0900

    wip commit add multiple scene(#1)

--- MyGame6/ChangeScene.cs
+++ MyGame6/ChangeScene.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Stride.Core.Mathematics;
 using Stride.Input;
 using Stride.Engine;
+using Stride.Core;
 
 namespace MyGame6
 {
@@ -13,25 +14,66 @@ namespace MyGame6
     {
         // Declared public member fields and properties will show in the game studio
         Scene CurrentScene;
+        Scene BepuScene;
+        Scene OriginalBodyScene;
+        Vector3 originalCubePosition;
+        Vector3 originalSpherePosition;
         public override void Start()
         {
             // Initialization of the script.
-            foreach(var scene in SceneSystem.SceneInstance.RootScene.Children)
-            {
-                if(scene.Name.Equals("BepuScene", StringComparison.OrdinalIgnoreCase))
-                {
-                    CurrentScene = scene;
-                    // SceneSystem.Ac
-                    break;
-                }
-            }
+            BepuScene = Content.Load<Scene>("BepuScene");
+            OriginalBodyScene = Content.Load<Scene>("OriginalBodyScene");
+            SceneSystem.SceneInstance.RootScene.Children.Add(BepuScene);
+            CurrentScene = BepuScene;
+            (originalCubePosition, originalSpherePosition) = GetPosition(CurrentScene);
         }
 
         public override void Update()
         {
-            if(Input.IsKeyPressed(Keys.D1))
+            if(Input.IsKeyPressed(Keys.D1) && CurrentScene != BepuScene)
             {
-                
+                SceneSystem.SceneInstance.RootScene.Children.Remove(CurrentScene);
+                SceneSystem.SceneInstance.RootScene.Children.Add(BepuScene);
+                CurrentScene = BepuScene;
+                ResetPosition(CurrentScene);
+            }
+            else if(Input.IsKeyPressed(Keys.D2) && CurrentScene != OriginalBodyScene)
+            {
+                SceneSystem.SceneInstance.RootScene.Children.Remove(CurrentScene);
+                SceneSystem.SceneInstance.RootScene.Children.Add(OriginalBodyScene);
+                CurrentScene = OriginalBodyScene;
+                ResetPosition(CurrentScene);
+            }
+        }
+        (Vector3 spherePosition, Vector3 cubePosition) GetPosition(Scene scene)
+        {
+            Vector3 spherePosition = Vector3.Zero;
+            Vector3 cubePosition = Vector3.Zero;
+            foreach(var entity in scene.Entities)
+            {
+                if(entity.Name == "Sphere")
+                {
+                    spherePosition = entity.Transform.Position;
+                }
+                else if(entity.Name == "Cube")
+                {
+                    cubePosition = entity.Transform.Position;
+                }
+            }
+            return (spherePosition, cubePosition);
+        }
+        void ResetPosition(Scene scene)
+        {
+            foreach(var entity in scene.Entities)
+            {
+                if(entity.Name == "Sphere")
+                {
+                    entity.Transform.Position = originalSpherePosition;
+                }
+                else if(entity.Name == "Cube")
+                {
+                    entity.Transform.Position = originalCubePosition;
+                }
             }
         }
     }
---
+++ MyGame6/MoveCubeOriginal.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+using Stride.BepuPhysics.Components.Containers;
+
+namespace MyGame6
+{
+    public class MoveCubeOriginal : SyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+
+        public override void Start()
+        {
+            // Initialization of the script.
+        }
+
+        public override void Update()
+        {
+            const float moveAmmount = 0.1f;
+            const float rotateAmount = 10f;
+            var moveVector = Vector3.Zero;
+            var rotation = Entity.Transform.Rotation;
+            if(Input.IsKeyDown(Keys.Left))
+            {
+                moveVector.X -= moveAmmount;
+            }
+            else if(Input.IsKeyDown(Keys.Right))
+            {
+                moveVector.X += moveAmmount;
+            }
+            if(Input.IsKeyDown(Keys.Up))
+            {
+                moveVector.Z -= moveAmmount;
+            }
+            else if(Input.IsKeyDown(Keys.Down))
+            {
+                moveVector.Z += moveAmmount;
+            }
+            if(Input.IsKeyDown(Keys.C))
+            {
+                moveVector.Y -= moveAmmount;
+            }
+            if(Input.IsKeyDown(Keys.U))
+            {
+                moveVector.Y += moveAmmount;
+            }
+            if(Input.IsKeyDown(Keys.Q))
+            {
+                rotation *= Quaternion.RotationZ(rotateAmount);
+            }
+            if(Input.IsKeyDown(Keys.E))
+            {
+                rotation *= Quaternion.RotationZ(-rotateAmount);
+            }
+            Entity.Transform.Position += moveVector;
+            Entity.Transform.Rotation = rotation;
+            Entity.Transform.UpdateLocalFromWorld();
+        }
+    }
+}
