e8870f5d899cb18b0b08054dd9a68be489755d9c
itn3000 <itn3000@gmail.com>
2023-12-20 17:53:58 +0900

    wip commit

--- MyGame6/ChangeScene.cs
+++ MyGame6/ChangeScene.cs
@@ -8,6 +8,7 @@ using Stride.Input;
 using Stride.Engine;
 using Stride.Core;
 using Stride.Physics;
+using Stride.BepuPhysics.Extensions;
 
 namespace MyGame6
 {
@@ -24,19 +25,29 @@ namespace MyGame6
             // Initialization of the script.
             BepuScene = Content.Load<Scene>("BepuScene");
             OriginalBodyScene = Content.Load<Scene>("OriginalBodyScene");
+            foreach(var scene in SceneSystem.SceneInstance.RootScene.Children)
+            {
+                SceneSystem.SceneInstance.RootScene.Children.Remove(scene);
+            }
             SceneSystem.SceneInstance.RootScene.Children.Add(BepuScene);
             CurrentScene = BepuScene;
             (originalSpherePosition, originalCubePosition) = GetPosition(CurrentScene);
         }
 
+        Scene LoadBepuScene() => Content.Load<Scene>("BepuScene");
+        Scene LoadOriginalScene() => Content.Load<Scene>("OriginalBodyScene");
+
         public override void Update()
         {
             if(Input.IsKeyPressed(Keys.D1) && CurrentScene != BepuScene)
             {
+                var old = BepuScene;
+                var loaded = LoadBepuScene();
                 SceneSystem.SceneInstance.RootScene.Children.Remove(CurrentScene);
-                SceneSystem.SceneInstance.RootScene.Children.Add(BepuScene);
+                SceneSystem.SceneInstance.RootScene.Children.Add(loaded);
+                BepuScene = loaded;
                 CurrentScene = BepuScene;
-                ResetPosition(CurrentScene);
+                //ResetPosition(CurrentScene);
             }
             else if(Input.IsKeyPressed(Keys.D2) && CurrentScene != OriginalBodyScene)
             {
@@ -52,11 +63,11 @@ namespace MyGame6
             Vector3 cubePosition = Vector3.Zero;
             foreach(var entity in scene.Entities)
             {
-                if(entity.Name == "Sphere")
+                if(entity.Name.StartsWith("Sphere"))
                 {
                     spherePosition = entity.Transform.Position;
                 }
-                else if(entity.Name == "Cube")
+                else if(entity.Name.StartsWith("Cube"))
                 {
                     cubePosition = entity.Transform.Position;
                 }
@@ -69,33 +80,48 @@ namespace MyGame6
             {
                 if(entity.Name.StartsWith("Sphere"))
                 {
-                    entity.Transform.Position = originalSpherePosition;
-                    //if(entity.Name.EndsWith("Bepu"))
+                    //entity.Transform.Position = originalSpherePosition;
+                    //if (entity.Name.EndsWith("Bepu"))
                     //{
                     //    var container = entity.Get<Stride.BepuPhysics.Components.Containers.BodyContainerComponent>();
                     //    //container.Position = originalSpherePosition;
+                    //    //var physic = container.GetPhysicBody();
+                    //    //if (physic.HasValue)
+                    //    //{
+                    //    //    physic.Value.Pose.Position = originalSpherePosition.ToNumericVector();
+                    //    //    physic.Value.UpdateBounds();
+                    //    //}
+
                     //}
                     //if (entity.Name.EndsWith("Original"))
                     //{
                     //    var rigid = entity.Get<RigidbodyComponent>();
                     //    rigid.UpdatePhysicsTransformation();
                     //}
-                    entity.Transform.UpdateWorldMatrix();
+                    //entity.Transform.UpdateWorldMatrix();
 
                 }
                 else if(entity.Name.StartsWith("Cube"))
                 {
                     entity.Transform.Position = originalCubePosition;
-                    //if (entity.Name.EndsWith("Bepu"))
-                    //{
-                    //    var container = entity.Get<Stride.BepuPhysics.Components.Containers.BodyContainerComponent>();
-                    //    //container.Position = originalCubePosition;
-                    //}
-                    //if (entity.Name.EndsWith("Original"))
-                    //{
-                    //    var rigid = entity.Get<RigidbodyComponent>();
-                    //    rigid.UpdatePhysicsTransformation();
-                    //}
+
+                    if (entity.Name.EndsWith("Bepu"))
+                    {
+                        var container = entity.Get<Stride.BepuPhysics.Components.Containers.BodyContainerComponent>();
+                        var collider = entity.Get<Stride.BepuPhysics.Components.Colliders.BoxColliderComponent>();
+                        //var physic = container.GetPhysicBody();
+                        //if(physic.HasValue)
+                        //{
+                        //    physic.Value.Pose.Position = originalCubePosition.ToNumericVector();
+                        //    physic.Value.UpdateBounds();
+                        //}
+                        //physic.Value.UpdateBounds();
+                    }
+                    if (entity.Name.EndsWith("Original"))
+                    {
+                        var rigid = entity.Get<RigidbodyComponent>();
+                        rigid.UpdatePhysicsTransformation();
+                    }
                     entity.Transform.UpdateWorldMatrix();
                 }
             }
---
+++ MyGame6/DisplayPosition.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stride.Core.Mathematics;
+using Stride.Input;
+using Stride.Engine;
+
+namespace MyGame6
+{
+    public class DisplayPosition : SyncScript
+    {
+        // Declared public member fields and properties will show in the game studio
+        public int PositionY = 60;
+        public int PositionX = 10;
+
+        public override void Start()
+        {
+        }
+
+        public override void Update()
+        {
+            DebugText.Print($"{Entity.Name}: {Entity.Transform.Position} | {Entity.Transform.Rotation}", new Int2(PositionX, PositionY));
+        }
+    }
+}
